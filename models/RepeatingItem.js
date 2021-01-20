94c0a8d71c46d90cccb467c19696c29dfc569ef2
SamehAmnoun <samehamnoun@gmail.com>
2021-01-20 17:31:55 +0200

    format: formatted the file

--- models/RepeatingItem.js
+++ models/RepeatingItem.js
@@ -1,26 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const RepeatingItemSchema = mongoose.Schema({
-    title:{
-        type: String,
-        required: true
+const RepeatingItemSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        
+      type: String,
     },
-    days: {
-        type: [Number],
-        required: true,
-    }
-
-},{
+    interval: {
+      type: mongoose.Schema.Types.ObjectId,
+      reference: 'RepeatingInterval',
+      required: true,
+    },
+  },
+  {
     strict: true,
     versionKey: false,
     timestamps: {
-        createdAt: "createdAt",
-        updatedAt: "updatedAt"
-    }
-})
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
 
-module.exports = mongoose.model('RepeatingItems',RepeatingItemSchema)
+module.exports = mongoose.model("RepeatingItem", RepeatingItemSchema);
--- routes/repeatingItems.js
+++ routes/repeatingItems.js
@@ -1,58 +1,58 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const RepeatingItem = require('../models/RepeatingItem');
+const RepeatingItem = require("../models/RepeatingItem");
 
-router.get('/', async (req,res) => {
-    try {
-        const items = await RepeatingItem.find();
-        res.json(items)
-    }catch (err){
-        res.json(err)
-    }
+router.get("/", async (req, res) => {
+  try {
+    const items = await RepeatingItem.find();
+    res.json(items);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.get('/:itemID', async (req,res) => {
-    try {
-        const item = await RepeatingItem.findById(req.params.itemID);
-        res.json(item)
-    }catch (err){
-        res.json(err)
-    }
+router.get("/:itemID", async (req, res) => {
+  try {
+    const item = await RepeatingItem.findById(req.params.itemID);
+    res.json(item);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
-router.post('/', async (req,res) => {
-    const item = new RepeatingItem({
-        title: req.body.title,
-        description: req.body.description,
-        days: req.body.days
-    });
-    try {
-        const savedItem = await item.save();
-        res.json(savedItem);
-    } catch (err) {
-        res.json({message: err});
-    }
-})
-
-router.delete('/:itemID', async (req,res) => {
-    try {
-        const itemToDelete = await RepeatingItem.remove({_id: req.params.itemID});
-        res.json(itemToDelete)
-    }catch (err){
-        res.json(err)
-    }
+router.post("/", async (req, res) => {
+  const item = new RepeatingItem({
+    title: req.body.title,
+    description: req.body.description,
+    interval: req.body.interval,
+  });
+  try {
+    const savedItem = await item.save();
+    res.json(savedItem);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
-router.patch('/:itemID', async (req,res) => {
-    try {
-        const itemToUpdate = await RepeatingItem.updateOne(
-            {_id: req.params.itemID},
-            {$set:{title: req.body.title}}
-        );
-        res.json(itemToUpdate)
-    }catch (err){
-        res.json(err)
-    }
+router.delete("/:itemID", async (req, res) => {
+  try {
+    const itemToDelete = await RepeatingItem.remove({ _id: req.params.itemID });
+    res.json(itemToDelete);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.patch("/:itemID", async (req, res) => {
+  try {
+    const itemToUpdate = await RepeatingItem.updateOne(
+      { _id: req.params.itemID },
+      { $set: { title: req.body.title } }
+    );
+    res.json(itemToUpdate);
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
