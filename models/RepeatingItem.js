f2d34584d0ff5c139bc93433d01bc026323af8a1
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-21 13:35:20 +0200

    feat: schema desc and string trim

--- models/RepeatingItem.js
+++ models/RepeatingItem.js
@@ -3,15 +3,19 @@ const mongoose = require("mongoose");
 const RepeatingItemSchema = mongoose.Schema(
   {
     title: {
+      desc: "Item Title",
       type: String,
       required: true,
     },
     description: {
+      desc: "Item Description",
       type: String,
+      trim: true,
     },
     interval: {
+      desc: "Item Interval",
       type: mongoose.Schema.Types.ObjectId,
-      reference: 'RepeatingInterval',
+      reference: "RepeatingInterval",
       required: true,
     },
   },
