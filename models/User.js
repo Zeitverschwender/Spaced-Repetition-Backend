66b8c902b620e60a7adf8f6cb9e214d47ba51fe2
SamehAmnoun <samehamnoun@gmail.com>
2021-01-25 17:15:28 +0200

    refactor: renamed repeatingitems to repeating Items

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema({
     image: {
         type: String
     },
-    repeatingitems: {
+    repeatingItems: {
         type: [RepeatingItem.schema]
     },
     customIntervals:{
